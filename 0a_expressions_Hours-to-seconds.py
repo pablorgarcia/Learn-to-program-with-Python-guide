f5f9ed689a0db9942573af566c708edd48d44755
PableraShow <pablo.ramon.garcia.fernandez@gmail.com>
2015-07-19 09:23:17 -0300

    Hours to second exercise

---
+++ 0a_expressions_Hours-to-seconds.py
@@ -0,0 +1,10 @@
+
+# Prints the number of seconds in 7 hours, 21 minutes and 37 seconds.
+
+# Hours, minutes, and seconds to seconds conversion formula
+min_in_7h = 7 * 60
+seg_in_7h_21min = (min_in_7h + 21) * 60
+seg_total = seg_in_7h_21min + 37
+
+# Output
+print seg_total
