1baf4bf9561bfdc9f31e4d720e2b760fa778c44c
PableraShow <pablo.ramon.garcia.fernandez@gmail.com>
2015-07-26 13:35:41 -0300

    improving code's understandability

--- logic-and-conditionals/modulus-operations.py
+++ logic-and-conditionals/modulus-operations.py
@@ -1,53 +1,59 @@
-# More Operations - placeholders for important values
-# Modulus
-
-
-# The modulus function is a form of division where the 
-#	remainder is returned. It is represented by the '%' symbol.
-
-print "Ex. 1:", 11 % 4
-print "Ex. 2:", 2 % 7
-print "Ex. 3:", 4 % 4
+print 'The modulus function is a form of division where the remainder is returned. It is represented by the % symbol.'
+print
+print "7 / 2 =", 7 / 2
+print "7 % 2 =", 7 % 2
+print
+print "11 / 4 =", 11 / 4
+print "11 % 4 =", 11 % 4
+print
+print "2 / 7 =", 2 / 7
+print "2 % 7 =", 2 % 7
+print
+print "4 / 4 =", 4 / 4
+print "4 % 4 =", 4 % 4
 print
 
-# Modulus can also be done on fractions and decimals.
-
-print "Ex. 4:", 5.2 % 3
-print "Ex. 5:", 2 % 1.5
-print "Ex. 6:", 3.5 % .5
-print "Ex. 7:", (5.0 / 4.0) % 3.0
-print "Ex. 8:", 2.75 % (1.0 / 2.0)
+print 'Modulus can also be done on fractions and decimals.'
+print
+print "5.2 / 3:", 5.2 / 3
+print "5.2 % 3:", 5.2 % 3
+print
+print "2 % 1.5:", 2 % 1.5
+print
+print "3.5 % .5:", 3.5 % .5
+print
+print "(5.0 / 4.0) % 3.0:", (5.0 / 4.0) % 3.0
+print
+print "2.75 % (1.0 / 2.0):", 2.75 % (1.0 / 2.0)
 print
 
-# Modulus is done at the same time as division and multiplication
-#	in the order of operations
-
-print "Ex. 9:", 5 + 3 % 2
-print "Ex. 10:", (5 + 3) % 2
+print 'Modulus is done at the same time as division and multiplication in the order of operations'
 print
-print "Ex. 11:", 6.0 / 3.0 % 5.0
-print "Ex. 12:", 6.0 % 5.0 / 3.0
+print "5 + 3 % 2 =", 5 + 3 % 2
+print "(5 + 3) % 2 =", (5 + 3) % 2
+print
+print "6.0 / 3.0 % 5.0 =", 6.0 / 3.0 % 5.0
+print "6.0 % 5.0 / 3.0 =", 6.0 % 5.0 / 3.0
 print
 
-# Remember that division in Python and CodeSkulptor is not
-#	exact.
-
-print "Ex. 13:", 5.4 % 3
+print 'Remember that division in Python and CodeSkulptor is not exact.'
+print
+print "5.4 % 3 =", 5.4 % 3
 # Almost zero...the e-17 means: * (10 ** -17), which is
 #	a really small number
-print "Ex. 14:", .9 % .3
+print ".9 % .3 =", .9 % .3
 
 
 print "--------"
-# Modulus can also be used with negative numbers. The
-#	first number dictates which direction you would move
-#	along a number line to get the answer, and the second
-#	number dictates which way the line is facing. 
+print 'Modulus can also be used with negative numbers.'
+print 'The first number dictates which direction you would move'
+print 'along a number line to get the answer, and the second'
+print 'number dictates which way the line is facing.'
 
 print 3 % 8, -3 % -8, -3 % 8, 3 % -8
 print
 
-# Number line is positive, direction is positive
+print 'Number line is positive, direction is positive'
 
 print 5 % 3, 2 % 5
 
@@ -65,3 +71,5 @@ print -5 % -3, -2 % -5
 # Same as ( -(b - (a % b)))
 
 print 5 % -3, 2 % -5
+
+
