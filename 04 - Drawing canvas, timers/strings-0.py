f0de716d6a7ae7ce56dc9f39bf98c9de40e36775
PableraShow <pablo.ramon.garcia.fernandez@gmail.com>
2015-08-10 19:00:49 -0300

    .

--- 04 - Drawing canvas, timers/strings-0.py
+++ 04 - Drawing canvas, timers/strings-0.py
@@ -11,10 +11,16 @@ print s3
 # Substrings
 print s3[5]
 print len(s3)
-print s3[0:5]
+print s3[0:4]
 
 # Converting strings
 s4 = str(367)
 print s4[1:3]
 i1 = int(s4[1:3])
 print i1 + 42
+
+
+
+num = 1234
+numStr = str(num)
+numStr
